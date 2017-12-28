3a4262b16f62418e6bfa9d4d1e8afed7b937d7ac
frankfoofoo <fuguoding@gmail.com>
2017-12-28 18:24:12 +0800

    新增例子

---
+++ how_to_scan_images/how_to_scan_images.cpp
@@ -0,0 +1,251 @@
+﻿#include <opencv2/core.hpp>
+#include <opencv2/core/utility.hpp>
+#include "opencv2/imgcodecs.hpp"
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+static void help()
+{
+	cout
+		<< "\n--------------------------------------------------------------------------" << endl
+		<< "This program shows how to scan image objects in OpenCV (cv::Mat). As use case"
+		<< " we take an input image and divide the native color palette (255) with the " << endl
+		<< "input. Shows C operator[] method, iterators and at function for on-the-fly item address calculation." << endl
+		<< "Usage:" << endl
+		<< "./how_to_scan_images <imageNameToUse> <divideWith> [G]" << endl
+		<< "if you add a G parameter the image is processed in gray scale" << endl
+		<< "--------------------------------------------------------------------------" << endl
+		<< endl;
+}
+
+Mat& ScanImageAndReduceC(Mat& I, const uchar *table);
+Mat& ScanImageAndReduceIterator(Mat& I, const uchar *table);
+Mat& ScanImageAndReduceRandomAccess(Mat& I, const uchar *table);
+
+int main(int argc, char* argv[])
+{
+	help();
+
+	bool isGrayImg = false;
+
+	Mat I, J;
+	if (isGrayImg)
+	{
+		I = imread("test_gray.png", IMREAD_GRAYSCALE);
+	}
+	else
+	{
+		I = imread("test.png", IMREAD_COLOR);
+	}
+
+	if (I.empty())
+	{
+		cout << "The image could not be loaded." << endl;
+		return -1;
+	}
+
+	imshow("test_before", I);
+
+	//! [dividewith]
+	int divideWith = 10; // convert our input string to number - C++ style
+	uchar table[256];
+	for (int i = 0; i < 256; ++i)
+	{
+		table[i] = (uchar)(divideWith * (i / divideWith));
+	}
+	//! [dividewith]
+
+	// 每一种方式测试次数
+	const int times = 1;
+	double t;
+
+	// getTickCount返回从操作系统启动到当前所经的计时周期数
+	// getTickFrequency返回每秒的计时周期数
+	t = (double)getTickCount();
+
+	for (int i = 0; i < times; ++i)
+	{
+		Mat clone_i = I.clone();
+		J = ScanImageAndReduceC(clone_i, table);
+	}
+
+	// 计算一共耗时多少毫秒
+	t = 1000 * ((double)getTickCount() - t) / getTickFrequency();
+	// 平均耗时多少毫秒
+	t /= times;
+
+	cout << "Time of reducing with the C operator [] (averaged for " << times << " runs): " << t << " milliseconds." << endl;
+
+	t = (double)getTickCount();
+
+	for (int i = 0; i < times; ++i)
+	{
+		Mat clone_i = I.clone();
+		J = ScanImageAndReduceIterator(clone_i, table);
+	}
+
+	t = 1000 * ((double)getTickCount() - t) / getTickFrequency();
+	t /= times;
+
+	cout << "Time of reducing with the iterator (averaged for " << times << " runs): " << t << " milliseconds." << endl;
+
+	t = (double)getTickCount();
+
+	for (int i = 0; i < times; ++i)
+	{
+		Mat clone_i = I.clone();
+		ScanImageAndReduceRandomAccess(clone_i, table);
+	}
+
+	t = 1000 * ((double)getTickCount() - t) / getTickFrequency();
+	t /= times;
+
+	cout << "Time of reducing with the on-the-fly address generation - at function (averaged for " << times << " runs): " << t << " milliseconds." << endl;
+
+	//! [table-init]
+	Mat lookUpTable(1, 256, CV_8U);
+	uchar *p = lookUpTable.ptr();
+	for (int i = 0; i < 256; ++i)
+	{
+		p[i] = table[i];
+	}
+	//! [table-init]
+
+	t = (double)getTickCount();
+
+	for (int i = 0; i < times; ++i)
+	{
+		//! [table-use]
+		LUT(I, lookUpTable, J);
+		//! [table-use]
+	}
+
+	t = 1000 * ((double)getTickCount() - t) / getTickFrequency();
+	t /= times;
+
+	cout << "Time of reducing with the LUT function (averaged for " << times << " runs): " << t << " milliseconds." << endl;
+
+	waitKey(0);
+
+	return 0;
+}
+
+//! [scan-c] C语言方式处理
+Mat& ScanImageAndReduceC(Mat& I, const uchar * const table)
+{
+	// accept only char type matrices
+	// 只接收8位无符号精度（每一个像素中每一个通道的）
+	CV_Assert(I.depth() == CV_8U);
+
+	// 获取通道数
+	int channels = I.channels();
+
+	// 获取行
+	int nRows = I.rows;
+	// 获取矩阵总列数，通道数乘以矩阵的列
+	int nCols = I.cols * channels;
+
+	// 判断像素点排列在内存中是否连续，如果连续转换成一行，提高执行效率
+	if (I.isContinuous())
+	{
+		nCols *= nRows;
+		nRows = 1;
+	}
+
+	int i, j;
+	uchar *p;
+	for (i = 0; i < nRows; ++i)
+	{
+		// 依次取出矩阵的每一行
+		p = I.ptr<uchar>(i);
+		for (j = 0; j < nCols; ++j)
+		{
+			// 依次取出每个通道值进行查表转换
+			p[j] = table[p[j]];
+		}
+	}
+
+	return I;
+}
+//! [scan-c]
+
+//! [scan-iterator] 迭代器方式处理
+Mat& ScanImageAndReduceIterator(Mat& I, const uchar* const table)
+{
+	// accept only char type matrices
+	CV_Assert(I.depth() == CV_8U);
+
+	const int channels = I.channels();
+	switch (channels)
+	{
+		case 1:
+		{
+			// 单通道
+			MatIterator_<uchar> it, end;
+			for (it = I.begin<uchar>(), end = I.end<uchar>(); it != end; ++it)
+			{
+				*it = table[*it];
+			}
+			break;
+		}
+		case 3:
+		{
+			// 3通道
+			MatIterator_<Vec3b> it, end;
+			for (it = I.begin<Vec3b>(), end = I.end<Vec3b>(); it != end; ++it)
+			{
+				(*it)[0] = table[(*it)[0]];
+				(*it)[1] = table[(*it)[1]];
+				(*it)[2] = table[(*it)[2]];
+			}
+		}
+	}
+
+	return I;
+}
+//! [scan-iterator]
+
+//! [scan-random]
+Mat& ScanImageAndReduceRandomAccess(Mat& I, const uchar* const table)
+{
+	// accept only char type matrices
+	CV_Assert(I.depth() == CV_8U);
+
+	const int channels = I.channels();
+	switch (channels)
+	{
+		case 1:
+		{
+			for (int i = 0; i < I.rows; ++i)
+			{
+				for (int j = 0; j < I.cols; ++j)
+				{
+					I.at<uchar>(i, j) = table[I.at<uchar>(i, j)];
+				}
+			}
+			break;
+		}
+		case 3:
+		{
+			Mat_<Vec3b> _I = I;
+
+			for (int i = 0; i < I.rows; ++i)
+			{
+				for (int j = 0; j < I.cols; ++j)
+				{
+					_I(i, j)[0] = table[_I(i, j)[0]];
+					_I(i, j)[1] = table[_I(i, j)[1]];
+					_I(i, j)[2] = table[_I(i, j)[2]];
+				}
+			}
+			I = _I;
+			break;
+		}
+	}
+
+	return I;
+}
+//! [scan-random]
---
+++ opencvlearn/opencvlearn.cpp
@@ -0,0 +1,136 @@
+﻿#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+void mat_init_example()
+{
+	Mat img0, img1;
+	// 读取测试图片
+	img0 = imread("test.png", IMREAD_COLOR);
+	// 初始化img2，只拷贝img0的结构体
+	Mat img2(img0);
+	// 初始化img1，只拷贝img0的结构体
+	img1 = img0;
+	// 初始化img3，取img0的一部分，矩形方法
+	Mat img3(img0, Rect(0, 0, 480, 480));
+	// 初始化img4，取img0的一部分，行和列方法
+	Mat img4 = img0(Range::all(), Range(0, 480));
+	// 初始化img5，clone方法
+	Mat img5 = img0.clone();
+	// 初始化img6，copyTo方法
+	Mat img6;
+	img0.copyTo(img6);
+
+	imshow("mat_init_example0", img0);
+	imshow("mat_init_example1", img1);
+	imshow("mat_init_example2", img2);
+	imshow("mat_init_example3", img3);
+	imshow("mat_init_example4", img4);
+	imshow("mat_init_example5", img5);
+	imshow("mat_init_example6", img6);
+
+	return;
+}
+
+void mat_create_example()
+{
+	// 创件一个矩阵，8行8列，3通道8位无符号，并初始化3通道的值（BGR排列）
+	Mat img0(8, 8, CV_8UC3, Scalar(255, 0, 0));
+	imshow("mat_create_example0", img0);
+	cout << "img0-0 = " << endl << img0 << endl << endl;
+
+	// 创建多维度的矩阵
+	int sz[3] = { 2, 2, 2 };
+	Mat img1(3, sz, CV_8UC(1), Scalar::all(0));
+
+	// 使用create方法创件矩阵
+	img0.create(4, 4, CV_8UC(3));
+	imshow("mat_create_example1", img0);
+	cout << "img0-1 = " << endl << img0 << endl << endl;
+
+	Mat img2;
+	img2.create(8, 8, CV_8UC(3));
+	imshow("mat_create_example2", img2);
+	cout << "img2 = " << endl << img2 << endl << endl;
+
+	// Matlab风格初始化
+	Mat O = Mat::ones(2, 2, CV_32F);
+	cout << "O = " << endl << O << endl << endl;
+	Mat Z = Mat::zeros(3, 3, CV_8UC1);
+	cout << "Z = " << endl << Z << endl << endl;
+	Mat E = Mat::eye(4, 4, CV_64F);
+	cout << "E = " << endl << E << endl << endl;
+
+	// 使用逗号分隔符初始化小型矩阵
+	Mat C = (Mat_<double>(3, 3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
+	cout << "C = " << endl << " " << C << endl << endl;
+	C = (Mat_<double>({ 0, -1, 0, -1, 5, -1, 0, -1, 0 })).reshape(3);
+	cout << "C = " << endl << " " << C << endl << endl;
+
+	// 为已经存在的矩阵创见一个头结构
+	Mat RowClone = C.row(0).clone();
+	cout << "RowClone = " << endl << " " << RowClone << endl << endl;
+
+	// 使用randu函数随机初始化矩阵值，并用不同格式化打印输出
+	Mat img3(3, 2, CV_8UC3);
+	randu(img3, Scalar::all(0), Scalar::all(255));
+	cout << "img3 (default)= " << endl << img3 << endl << endl;
+	cout << "img3 (python)  = " << endl << format(img3, Formatter::FMT_PYTHON) << endl << endl;
+	cout << "img3 (csv)  = " << endl << format(img3, Formatter::FMT_CSV) << endl << endl;
+	cout << "img3 (numpy)  = " << endl << format(img3, Formatter::FMT_NUMPY) << endl << endl;
+	cout << "img3 (c)  = " << endl << format(img3, Formatter::FMT_C) << endl << endl;
+
+	// 打印二维点坐标
+	Point2f P(5.0, 1.2);
+	cout << "Point (2D) = " << P << endl << endl;
+
+	// 打印三维点坐标
+	Point3f P3f(2, 6, 7);
+	cout << "Point (3D) = " << P3f << endl << endl;
+
+	vector<float> v;
+	v.push_back((float)CV_PI);
+	v.push_back(2);
+	v.push_back(3.01f);
+	cout << "Vector of floats via Mat = " << Mat(v) << endl << endl;
+
+	vector<Point2f> vPoints(20);
+	for (size_t i = 0; i < vPoints.size(); ++i)
+	{
+		vPoints[i] = Point2f((float)(i * 5), (float)(i % 7));
+	}
+	cout << "A vector of 2D Points = " << vPoints << endl << endl;
+
+	return;
+}
+
+void image_op_example()
+{
+	Mat img0 = imread("test.png", IMREAD_COLOR);
+	Mat img0_gray;
+
+	// 颜色空间转换，将BGR转为灰度图
+	cvtColor(img0, img0_gray, COLOR_BGR2GRAY);
+	// 写本地文件
+	imwrite("test_gray.png", img0_gray);
+
+	namedWindow("img0", WINDOW_AUTOSIZE);
+	namedWindow("img0_gray", WINDOW_AUTOSIZE);
+
+	imshow("img0", img0);
+	imshow("img0_gray", img0_gray);
+	
+	return;
+}
+
+int main(int argc, char **argv)
+{
+	//mat_init_example();
+	//mat_create_example();
+	image_op_example();
+
+	waitKey(0);
+
+	return 0;
+}
